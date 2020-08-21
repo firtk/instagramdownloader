5eb1293fbac4f453bf6ba66b1628fc9a6fc72d00
firtk <firtkilic_25@hotmail.com>
2020-08-22 01:09:58 +0300

    Multiple photos download feature

--- instagram_downloader.py
+++ instagram_downloader.py
@@ -3,11 +3,12 @@ import re
 import os
 import argparse
 import pathlib
-import html
+import json
+import urllib
 from urllib.request import urlopen, urlretrieve
 
 # Regular Expressions for retrieving data from page source code.
-PHOTO_EXPRESSION = r'<meta property="og:image" content="(.*?)" />'
+PHOTOS_EXPRESSION = r'"display_resources":(.*?)]'
 VIDEO_EXPRESSION = r'<meta property="og:video" content="(.*?)" />'
 CONTENT_EXPRESSION = r'<meta content="(.*?)"' # Needed for username
 USERNAME_EXPRESSION = r'\@.*?(\s|\))'
@@ -31,37 +32,36 @@ def get_page_source(url):
 
     return source
 
-def download_photo(url, path):
-    # Downloads posted photo to specified path or where does this script exists.
+def download_photos(url, path):
+    # Downloads posted photo or photos to specified path or where does this script exists.
     # Then opens it.
     page_source = get_page_source(url)
 
     content = re.search(CONTENT_EXPRESSION, page_source).group(0)
     username = re.search(USERNAME_EXPRESSION, content).group(0)[1:-1]
-    image_url = re.search(PHOTO_EXPRESSION, page_source).group(1)
-    image_url = html.unescape(image_url) # Decodes special characters
-    if path:
-        save_path = str(pathlib.PurePath(path, '{}_photo.jpeg'.format(username)))
-    else:
-        save_path = '{}_photo.jpeg'.format(username)
+    display_resources = re.findall(PHOTOS_EXPRESSION, page_source) # find all display resources
+    display_resources = [json.loads("{}]".format(resource)) for resource in display_resources] # Close bracket and turn str to list with json.loads
+    img_urls = [sorted(resource, key=lambda x: x.get('config_width'), reverse=True)[0]['src'] for resource in display_resources] # Get only highest resolution url for images
+    img_urls = [urllib.parse.unquote(url).encode().decode('unicode_escape') for url in img_urls] # Decode special characters such as \u0026 => &
+    img_urls = dict.fromkeys(img_urls).keys() # Remove occurence but keep order. # set() not used because it is disorders the list.
+    for i, url in enumerate(img_urls, start=1):
+        with urlopen(url, timeout=5) as response:
+            page_source = response.read()
+        with open(str(pathlib.PurePath(path, '{}_photo_{}.jpeg'.format(username, i))), 'wb') as img_file:
+            img_file.write(page_source)
 
-    with urlopen(image_url, timeout=5) as response:
-        page_source = response.read()
-    print("Download starting...")
-    with open(save_path, 'wb') as img_file:
-        img_file.write(page_source)
-
-    os.startfile(save_path)
+    os.startfile(str(pathlib.PurePath(path, '{}_photo_1.jpeg'.format(username)))) # Open first image.
 
 def download_video(url, path):
     # Downloads posted video to specified path or where does this script exists.
     # Then opens it.
+    # Note : It's also downloads video thumbnail as photo too.
     page_source = get_page_source(url)
 
     content = re.search(CONTENT_EXPRESSION, page_source).group(0)
     username = re.search(USERNAME_EXPRESSION, content).group(0)[1:-1]
     video_url = re.search(VIDEO_EXPRESSION, page_source).group(1)
-    video_url = html.unescape(video_url) # Decodes special characters
+    video_url = urllib.parse.unquote(video_url).encode().decode('unicode_escape') # Decode special characters such as \u0026 => &
     if path:
         save_path = str(pathlib.PurePath(path, '{}_video.mp4'.format(username)))
     else:
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         print("Something goes wrong, please check your url again.")
 
     try:
-        download_photo(url, path)
+        download_photos(url, path)
     except AttributeError:
         print("This post doesn't contain photo. Check your url please.")
     except Exception as err:
